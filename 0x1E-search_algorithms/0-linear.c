19ab933ca6e1f6c318229cc099185bcefbdcf66d
Mia-Tee <tanyatsekenie@gmail.com>
2024-02-10 15:54:44 -0800

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,18 +1,53 @@
-#include "search_algos.h"
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
-int linear_search(int *array, size_t size, int value)
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - a singly linked list
+ *
+ * @n: an integer
+ * @index: an index of the node in the list
+ * @next: a pointer to the next node
+ *
+ * Description: a singly linked list node structure
+ * Holberton project
+ */
+typedef struct listint_s
 {
-	size_t i;
+		int n;
+		size_t index;
+		struct listint_s *next;
+} listint_t;
 
-	if (array == NULL)
-		return (-1);
+/**
+ * struct skiplist_s - a singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: an index of the node in the list
+ * @next: a pointer to the next node
+ * @express: a pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * Holberton project
+ */
+typedef struct skiplist_s
+{
+				int n;
+				size_t index;
+				struct skiplist_s *next;
+				struct skiplist_s *express;
+} skiplist_t;
 
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
-	return (-1);
-}
+#endif /* SEARCH_ALGOS_H */
